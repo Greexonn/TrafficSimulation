4a2c2f235508eda6249c419c4e245ab936a9ef30
Nikita <smirnyh_ns@zrnc.ru>
2020-01-09 16:12:52 +0300

    Added road node

---
+++ Assets/Scripts/Traffic/Road/RoadNodeAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class RoadNodeAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    [SerializeField] private RoadNodeAuthoring _nextNode;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        RoadNodeComponent _nodeComponent = new RoadNodeComponent
+        {
+            nextNode = _nextNode != null ? conversionSystem.GetPrimaryEntity(_nextNode.gameObject) : Entity.Null,
+            isAvalible = true
+        };
+
+        dstManager.AddComponentData(entity, _nodeComponent);
+    }
+}
