d21b3b61a1879599e6c814b941944083acd4f219
Nikita <smirnyh_ns@zrnc.ru>
2020-01-14 12:41:09 +0300

    Added spawner component

---
+++ Assets/Scripts/Traffic/RoadComponents/CarSpawnerAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class CarSpawnerAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    [SerializeField] private RoadNodeAuthoring _spawnNode;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponent(conversionSystem.GetPrimaryEntity(_spawnNode.gameObject), typeof(CarSpawnerComponent));
+    }
+}
