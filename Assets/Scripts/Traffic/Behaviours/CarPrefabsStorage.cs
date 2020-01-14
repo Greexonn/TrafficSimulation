17560ff2af49620eaba385ee9c15d0f60f32527b
Nikita <smirnyh_ns@zrnc.ru>
2020-01-14 14:12:50 +0300

    Added spawn system

---
+++ Assets/Scripts/Traffic/Behaviours/CarPrefabsStorage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+
+public class CarPrefabsStorage : MonoBehaviour
+{
+    public static CarPrefabsStorage instance;
+
+    [SerializeField] private List<GameObject> _carPrefabs;
+
+    public NativeArray<Entity> carPrefabs;
+
+    void Awake()
+    {
+        if (instance != this)
+        {
+            if (instance != null)
+                Destroy(this);
+            else
+                instance = this;
+        }
+    }
+
+    void Start()
+    {
+        GameObjectConversionSettings _convSettings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        carPrefabs = new NativeArray<Entity>(_carPrefabs.Count, Allocator.Persistent);
+
+        for (int i = 0; i < _carPrefabs.Count; i++)
+        {
+            carPrefabs[i] = GameObjectConversionUtility.ConvertGameObjectHierarchy(_carPrefabs[i], _convSettings);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (carPrefabs.IsCreated)
+            carPrefabs.Dispose();
+    }
+}
---
+++ Assets/Scripts/Traffic/RoadSystems/CarSpawnSystem.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class CarSpawnSystem : ComponentSystem
+{
+    private float _secondsTillSpawn = 3;
+
+    private float _lastSpawnTime = 0;
+
+    private EntityManager _manager;
+
+
+    protected override void OnCreate()
+    {
+        _manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    }
+
+    protected override void OnUpdate()
+    {
+        float _currentTime = (float)Time.ElapsedTime;
+
+        if ((_currentTime - _lastSpawnTime) < _secondsTillSpawn)
+            return;
+
+        Entities.ForEach((ref CarSpawnerComponent spawner, ref LocalToWorld transform) => 
+        {
+            int _index = UnityEngine.Random.Range(0, CarPrefabsStorage.instance.carPrefabs.Length);
+
+            Entity _carEntity = _manager.Instantiate(CarPrefabsStorage.instance.carPrefabs[_index]);
+
+            _manager.SetComponentData(_carEntity, new Translation{Value = transform.Position});
+        });
+
+        _lastSpawnTime = _currentTime;
+    }
+}
