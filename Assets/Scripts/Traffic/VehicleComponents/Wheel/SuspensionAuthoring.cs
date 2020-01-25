febb2c1c27e94d6571683ecf70ae933393d9d770
Nikita <smirnyh_ns@zrnc.ru>
2020-01-25 10:02:05 +0300

    Added gizmos for wheel

--- Assets/Scripts/Traffic/VehicleComponents/Wheel/SuspensionAuthoring.cs
+++ Assets/Scripts/Traffic/VehicleComponents/Wheel/SuspensionAuthoring.cs
@@ -25,20 +25,27 @@ public class SuspensionAuthoring : MonoBehaviour, IConvertGameObjectToEntity
         Vector3 _toPos = transform.position + (transform.up * _suspension.suspensionLength / 2);
         Gizmos.color = Color.green;
         Gizmos.DrawLine(_fromPos, _toPos);
+        //draw suspension ends
+        Vector3 _fromEnd = _fromPos - (transform.right * _suspension.suspensionLength / 10);
+        Vector3 _toEnd = _fromPos + (transform.right * _suspension.suspensionLength / 10);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_fromEnd, _toEnd);
+        _fromEnd = _toPos - (transform.right * _suspension.suspensionLength / 10);
+        _toEnd = _toPos + (transform.right * _suspension.suspensionLength / 10);
+        Gizmos.DrawLine(_fromEnd, _toEnd);
         //place model in pos
         Vector3 _wheelCenter = Vector3.Lerp(_fromPos, _toPos, _suspension.wheelPosition);
-        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(_wheelCenter, (_suspension.suspensionLength / 20));
         if (_wheelModel != null)
         {
             _wheelModel.position = _wheelCenter;
         }
         //draw wheel
-        Gizmos.color = Color.green;
+        UnityEditor.Handles.color = Color.green;
         if (_wheel != null)
         {
             float _radius = _wheel.wheel.wheelRadius;
-            Gizmos.DrawWireSphere(_wheelCenter, _radius);
+            UnityEditor.Handles.DrawWireDisc(_wheelCenter, transform.forward, _radius);
         }
         else
             _wheel = GetComponent<WheelAuthoring>();
