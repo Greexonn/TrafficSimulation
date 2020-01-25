a535f6b9c93674b1b50c8e2b5770230c1ee45f02
Nikita <smirnyh_ns@zrnc.ru>
2020-01-25 10:42:24 +0300

    Added vehicle steering component

---
+++ Assets/Scripts/Traffic/VehicleComponents/DriveVehicle/VehicleSteeringComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct VehicleSteeringComponent : ISharedComponentData
+{
+    public float currentAngle;
+    public float maxAngle;
+    public float steeringSpeed;
+}
