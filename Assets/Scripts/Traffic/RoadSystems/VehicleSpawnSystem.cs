2f60ee1b850c633d3ed3d30580f0b38f5e81bb81
Nikita <smirnyh_ns@zrnc.ru>
2020-02-07 11:00:58 +0300

    Vehicle spawn system updated

--- Assets/Scripts/Traffic/RoadSystems/VehicleSpawnSystem.cs
+++ Assets/Scripts/Traffic/RoadSystems/VehicleSpawnSystem.cs
@@ -5,7 +5,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-public class CarSpawnSystem : ComponentSystem
+public class VehicleSpawnSystem : ComponentSystem
 {
     private float _secondsTillSpawn = 3;
 
@@ -28,13 +28,15 @@ public class CarSpawnSystem : ComponentSystem
 
         Entities.ForEach((Entity spawnerEntity, ref CarSpawnerComponent spawner, ref LocalToWorld transform) => 
         {
+            var _position = transform.Position + (math.up() * 1.0f);
+
             int _index = UnityEngine.Random.Range(0, CarPrefabsStorage.instance.carPrefabs.Length);
 
             Entity _vehicleEntity = _manager.Instantiate(CarPrefabsStorage.instance.carPrefabs[_index]);
             #if UNITY_EDITOR
             _manager.SetName(_vehicleEntity, "vehicle");
             #endif
-            _manager.SetComponentData(_vehicleEntity, new Translation{Value = transform.Position});
+            _manager.SetComponentData(_vehicleEntity, new Translation{Value = _position});
             _manager.AddComponentData(_vehicleEntity, new VehicleCurrentNodeComponent{node = spawnerEntity});
             _manager.AddComponentData(_vehicleEntity, new VehiclePathNodeIndexComponent{value = 0});
             _manager.AddBuffer<NodeBufferElement>(_vehicleEntity);
